659607534696810f6d76bfe640deedfd010f2a2a
kejuuu <kelvinjulian.kj@gmail.com>
2020-09-17 17:51:22 +0700

    - Modified: DatabaseMain.cs, added list and functionalities to actors tab

--- Editor/DatabaseMain.cs
+++ Editor/DatabaseMain.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(ActorData))]
@@ -106,8 +107,28 @@ public class DatabaseMain : EditorWindow
 
     #region Functionality Tab  
     //////////////////////////////////////////////////
+    bool once = false;
+    public int actorSize;
+    public int actorSizeTemp;
+
+    public class Player
+    {
+        public string name;
+        public int damage;
+    }
+    List<Player> player = new List<Player>();
+
+    List<string> playerName = new List<string>();
+    int index = 0;
+    int indexTemp = -1;
+    Vector2 scrollPos = Vector2.zero;
     private void ActorsTab()
     {
+        if(!once)
+        {
+            onceCalled();
+        }
+
         //Initialize value that actor tab needed.
         GUIStyle actorStyle;
 
@@ -118,7 +139,7 @@ public class DatabaseMain : EditorWindow
         float firstTabWidth = tabWidth * 3 / 10;
 
         //Size array of Actor
-        int actorSize = 4;
+        //actorSize = 4;
 
         actorStyle = new GUIStyle(GUI.skin.box);
         actorStyle.normal.background = CreateTexture(1, 1, Color.gray);
@@ -132,21 +153,75 @@ public class DatabaseMain : EditorWindow
         //First Tab
         GUILayout.BeginArea(new Rect(0, 0, tabWidth, tabHeight));
         GUILayout.Box("Actors", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15));
-        actorSize = EditorGUILayout.IntField(actorSize, GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10));
-        GUILayout.Button("Change Maximum", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10));
 
-        EditorGUILayout.PropertyField(actorName, new GUIContent("Actor Name"), GUILayout.Height(20));
+        //Scroll View
+        scrollPos = GUILayout.BeginScrollView(scrollPos, false, true, GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .82f));
+        index = GUILayout.SelectionGrid(index, playerName.ToArray(), 1, GUILayout.Width(firstTabWidth-20), GUILayout.Height(30*actorSize));
+        GUILayout.EndScrollView();
+
+        //Happen everytime selection grid is updated
+        if(GUI.changed && index != indexTemp)
+        {
+            indexTemp = index;
+            chooseActor(indexTemp);
+            indexTemp = -1;
+        }
+        
+
+        //if (GUILayout.Button("Choose!", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10))) { chooseActor(index); }
+
+        actorSizeTemp = EditorGUILayout.IntField(actorSizeTemp, GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10));
+        if(GUILayout.Button("Change Maximum", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10)))
+        {
+            actorSize = actorSizeTemp;
+            once = false;
+        }
+
+
+
+        //EditorGUILayout.PropertyField(actorName, new GUIContent("Actor Name"), GUILayout.Height(20));
+
+
+
 
         GUILayout.EndArea();
+
         GUILayout.EndArea();
     }
 
+    public void chooseActor(int index)
+    {
+        if(index!=-1)
+        {
+            Debug.Log(player[index].name);
+        }
+    }
+
+    int counter = 0;
+    public void onceCalled()
+    {
+        while(counter <= actorSize)
+        {
+            player.Add(new Player { name = "keju" + counter.ToString(), damage = 1 });
+            playerName.Add(player[counter].name);
+            counter++;
+            once = true;
+        }
+        if(counter > actorSize)
+        {
+            player.RemoveRange(actorSize, player.Count - actorSize);
+            playerName.RemoveRange(actorSize, playerName.Count - actorSize);
+            counter = actorSize;
+        }
+        once = true;
+    }
     //////////////////////////////////////////////////
 
     private void ClassesTab()
     {
 
     }
+
 
     //////////////////////////////////////////////////
     #endregion
