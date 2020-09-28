6462074f808a88cdcef0385d62723cf0f6d3cc3c
kennetha123 <jodysag5@gmail.com>
2020-09-28 09:00:30 +0700

    - Fixing merge conflict text

--- Editor/SkillsTab.cs
+++ Editor/SkillsTab.cs
@@ -6,10 +6,8 @@ using SFB;
 
 public class SkillsTab
 {
-<<<<<<< HEAD
     //All GUIStyle variable initialization.
     GUIStyle skillStyle;
-=======
     //Having list of all skills exist in data.
     public List<SkillData> skill = new List<SkillData>();
 
@@ -20,7 +18,6 @@ public class SkillsTab
     List<string> skillDisplayName = new List<string>();
 
     //All GUIStyle variable initialization.
-    GUIStyle skillStyle;
     GUIStyle tabStyle;
     GUIStyle columnStyle;
 
@@ -97,8 +94,6 @@ public class SkillsTab
     Texture2D Icon;
 
     public int skillSizeTemp;
->>>>>>> 6e99564... + Add: SkillsTab.cs SkillData.cs ,Modified: DatabaseMain.cs
-
     public void Init(Rect position)
     {
         #region A Bit Explanation About Local Tab
@@ -116,24 +111,11 @@ public class SkillsTab
         float tabWidth = position.width * .85f;
         float tabHeight = position.height - 10f;
 
-<<<<<<< HEAD
         //Style area.
         skillStyle = new GUIStyle(GUI.skin.box);
-        skillStyle.normal.background = CreateTexture(1, 1, Color.gray);
-=======
+
         float firstTabWidth = tabWidth * 3 / 10;
 
-        //Style area.
-        skillStyle = new GUIStyle(GUI.skin.box);
-        skillStyle.normal.background = CreateTexture(1, 1, Color.gray);
-        columnStyle = new GUIStyle(GUI.skin.box);
-        columnStyle.normal.background = CreateTexture(1, 1, new Color32(99, 100, 100, 200));
-        tabStyle = new GUIStyle(GUI.skin.box);
-        if (EditorGUIUtility.isProSkin)
-            tabStyle.normal.background = CreateTexture(1, 1, new Color32(76, 76, 76, 200));
-        else
-            tabStyle.normal.background = CreateTexture(1, 1, new Color32(200, 200, 200, 200));
->>>>>>> 6e99564... + Add: SkillsTab.cs SkillData.cs ,Modified: DatabaseMain.cs
 
         ////////////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////END REGION OF VALUE INIT///////////////////////////////////
@@ -143,240 +125,11 @@ public class SkillsTab
         //Start drawing the whole SkillsTab.
         GUILayout.BeginArea(new Rect(position.width / 7, 5, tabWidth, tabHeight));
 
-            //The black box behind the SkillsTab? yes, this one.
-            GUILayout.Box(" ", skillStyle, GUILayout.Width(position.width - DatabaseMain.tabAreaWidth), GUILayout.Height(position.height - 25f));
-<<<<<<< HEAD
-            
-            
+        //The black box behind the SkillsTab? yes, this one.
+        GUILayout.Box(" ", skillStyle, GUILayout.Width(position.width - DatabaseMain.tabAreaWidth), GUILayout.Height(position.height - 25f));
+
+
         GUILayout.EndArea(); // End of drawing SkillsTab
-
         #endregion
-=======
-
-            #region Tab 1/3
-            //First Tab of three
-            GUILayout.BeginArea(new Rect(0, 0, tabWidth, tabHeight));
-            GUILayout.Box("Skills", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15));
-
-            //Scroll View
-            #region ScrollView
-            scrollPos = GUILayout.BeginScrollView(scrollPos, false, true, GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .82f));
-            index = GUILayout.SelectionGrid(index, skillDisplayName.ToArray(), 1, GUILayout.Width(firstTabWidth - 20), GUILayout.Height(position.height / 24 * skillSize));
-            GUILayout.EndScrollView();
-            #endregion
-
-            //Happen everytime selection grid is updated
-            if (GUI.changed && index != indexTemp)
-            {
-                indexTemp = index;
-                SkillListSelected(indexTemp);
-                indexTemp = -1;
-            }
-
-            skillSizeTemp = EditorGUILayout.IntField(skillSizeTemp, GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10));
-            if (GUILayout.Button("Change Maximum", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15 - 10)))
-            {
-                ChangeMaximum(skillSize);
-            }
-            GUILayout.EndArea();
-            #endregion
-            #endregion
-
-            #region Tab 2/3
-            //second Column
-            GUILayout.BeginArea(new Rect(firstTabWidth + 5, 0, firstTabWidth + 70, tabHeight - 15), columnStyle);
-
-                //GeneralSettings tab
-                Rect generalBox = new Rect(5, 5, firstTabWidth + 60, position.height / 3 - 50);
-
-                #region GeneralSettings
-                GUILayout.BeginArea(generalBox, tabStyle); //Start of general settings tab
-                    GUILayout.Label("General Settings", EditorStyles.boldLabel); //general settings label
-                        GUILayout.BeginVertical();
-                            GUILayout.BeginHorizontal();
-                                GUILayout.BeginVertical(); //Name label, name field, class label, and class popup
-                                GUILayout.Label("Name:");
-                                if (skillSize > 0)
-                                {
-                                    skill[index].skillName = GUILayout.TextField(skill[index].skillName, GUILayout.Width(generalBox.width / 2 - 15), GUILayout.Height(generalBox.height / 8));
-                                    skillDisplayName[index] = skill[index].skillName;
-                                }
-                                else
-                                { GUILayout.TextField("Null", GUILayout.Width(generalBox.width / 2 - 15), GUILayout.Height(generalBox.height / 8)); }
-                                GUILayout.EndVertical(); //Name label, name field, class label, and class popup (ending)
-            
-                                /*GUILayout.BeginVertical(); //Nickname label, nickname field, initial level and max level label and field
-                                                           //put icon here
-                                GUILayout.EndVertical();*/
-                            GUILayout.EndHorizontal();
-                        GUILayout.EndVertical();
-
-                    GUILayout.EndArea(); //End of general settings tab
-                #endregion
-                GUILayout.EndArea(); //End of second column
-            #endregion
-            GUILayout.EndArea(); // End of drawing SkillsTab
->>>>>>> 6e99564... + Add: SkillsTab.cs SkillData.cs ,Modified: DatabaseMain.cs
-
     }
-
-    #region Features
-    /// <summary>
-    /// Create Texture for GUI skin.
-    /// </summary>
-    /// <param name="width">pixel width of GUI Skin.</param>
-    /// <param name="height">pixel height of GUI Skin.</param>
-    /// <param name="col">Color of GUI Skin.</param>
-    /// <returns></returns>
-    private Texture2D CreateTexture(int width, int height, Color col)
-    {
-        //Create array of color.
-        Color[] colPixel = new Color[width * height];
-
-        for (int i = 0; i < colPixel.Length; ++i)
-        {
-            colPixel[i] = col;
-        }
-
-        Texture2D result = new Texture2D(width, height);
-        result.SetPixels(colPixel);
-        result.Apply();
-        return result;
-    }
-
-<<<<<<< HEAD
-=======
-    /// <summary>
-    /// Change Maximum function , when we change the size
-    /// and click Change Maximum button in Editor, it will update
-    /// and change the size while creating new data.
-    /// </summary>
-    /// <param name="size">get size from skillSize</param>
-
-    int counter = 0;
-    private void ChangeMaximum(int size)
-    {
-        skillSize = skillSizeTemp;
-        //This count only useful when we doesn't have a name yet.
-        //you can remove this when decide a new format later.
-        while (counter <= skillSize)
-        {
-            skill.Add(ScriptableObject.CreateInstance<SkillData>());
-            AssetDatabase.CreateAsset(skill[counter], "Assets/Resources/Data/SkillData/Skill_" + counter + ".asset");
-            AssetDatabase.SaveAssets();
-            skillDisplayName.Add(skill[counter].skillName);
-            counter++;
-        }
-        if (counter > skillSize)
-        {
-            skill.RemoveRange(skillSize, skill.Count - skillSize);
-            skillDisplayName.RemoveRange(skillSize, skill.Count - skillSize);
-            for (int i = skillSize; i <= counter; i++)
-            {
-                AssetDatabase.DeleteAsset("Assets/Resources/Data/SkillData/Skill_" + i + ".asset");
-            }
-            AssetDatabase.SaveAssets();
-            counter = skillSize;
-        }
-    }
->>>>>>> 6e99564... + Add: SkillsTab.cs SkillData.cs ,Modified: DatabaseMain.cs
-
-    ///<summary>
-    /// Folder checker, create folder if it doesnt exist already, Might refactor into one liner if
-    ///</summary>
-    public void FolderChecker()
-    {
-        if (!AssetDatabase.IsValidFolder("Assets/Resources"))
-        {
-            AssetDatabase.CreateFolder("Assets", "Resources");
-        }
-        if (!AssetDatabase.IsValidFolder("Assets/Resources/Data"))
-        {
-            AssetDatabase.CreateFolder("Assets/Resources", "Data");
-        }
-        if (!AssetDatabase.IsValidFolder("Assets/Resources/Data/ActorData"))
-        {
-            AssetDatabase.CreateFolder("Assets/Resources/Data", "ActorData");
-        }
-        if (!AssetDatabase.IsValidFolder("Assets/Resources/Image"))
-        {
-            AssetDatabase.CreateFolder("Assets/Resources", "Image");
-        }
-        if (!AssetDatabase.IsValidFolder("Assets/Resources/Data/ClassesData"))
-        {
-            AssetDatabase.CreateFolder("Assets/Resources/Data", "ClassesData");
-        }
-        if (!AssetDatabase.IsValidFolder("Assets/Resources/Data/SkillData"))
-        {
-            AssetDatabase.CreateFolder("Assets/Resources/Data", "SkillData");
-        }
-
-    }
-
-<<<<<<< HEAD
-=======
-    ExtensionFilter[] extensions = new[] {
-        new ExtensionFilter("Image Files", "png", "jpg", "jpeg" ),
-        new ExtensionFilter("Sound Files", "mp3", "wav" ),
-        new ExtensionFilter("All Files", "*" ),
-    };
-
-    private void changeIconImage()
-    {
-        string relativepath;
-        string[] path = StandaloneFileBrowser.OpenFilePanel("Choose Icon", "Assets/Resources/Image", extensions, false);
-
-        if (path.Length != 0)
-        {
-            relativepath = "Image/";
-            relativepath += Path.GetFileNameWithoutExtension(path[0]);
-            Sprite imageChosen = Resources.Load<Sprite>(relativepath);
-            skill[index].skillIcon = imageChosen;
-            SkillListSelected(index);
-        }
-    }
-
-    /// <summary>
-    /// This called when actor list on active.
-    /// </summary>
-    /// <param name="index">index of actor in a list.</param>
-    public void SkillListSelected(int index)
-    {
-            
-        Texture2D defTex = new Texture2D(256, 256);
-        if (index != -1)
-        {
-            if (skillSize > 0)
-            {
-                if (skill[index].skillIcon == null)
-                    Icon = defTex;
-                else
-                    Icon = textureFromSprite(skill[index].skillIcon);
-            }
-        }
-    }
->>>>>>> 6e99564... + Add: SkillsTab.cs SkillData.cs ,Modified: DatabaseMain.cs
-
-    /// <summary>
-    /// Create a texture from a sprite (Used for changing actors' images)
-    /// </summary>
-    /// <param name="sprite">the sprite that wants to be converted into texture</param>
-    /// <returns></returns>
-    public static Texture2D textureFromSprite(Sprite sprite)
-    {
-        if (sprite.rect.width != sprite.texture.width)
-        {
-            Texture2D newText = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
-            Color[] newColors = sprite.texture.GetPixels((int)sprite.textureRect.x,
-                                                         (int)sprite.textureRect.y,
-                                                         (int)sprite.textureRect.width,
-                                                         (int)sprite.textureRect.height);
-            newText.SetPixels(newColors);
-            newText.Apply();
-            return newText;
-        }
-        else
-            return sprite.texture;
-    }
-    #endregion
 }
