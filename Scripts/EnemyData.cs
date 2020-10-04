609e4741ff497adf9d2f536c2203a22e250beab5
ZivFerdinand <zivenf@gmail.com>
2020-10-04 21:25:21 +0700

    - Modified: EnemyTab.cs EnemyData.cs
    - Modified: EnemyTab.cs, Added General Settings Box
    - Modified: EnemyData.cs, Added some variables
    
    Link: https://trello.com/c/VkG9Owjq/50-editor-tools-enemytab-general-settings

--- Scripts/EnemyData.cs
+++ Scripts/EnemyData.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class EnemyData : ScriptableObject
 {
     public string enemyName;
+    public Sprite Image;
+
+    public int enemyAttack;
+    public int enemyDefense;
+    public int enemyMAttack;
+    public int enemyMDefense;
+    public int enemyAgility;
+    public int enemyLuck;
+    public int enemyMaxHP;
+    public int enemyMaxMP;
+    public int enemyEXP;
+    public int enemyGold;
 
     public void OnEnable()
     {
         Sprite sp = Resources.Load<Sprite>("Image");
-
         enemyName = "enemy";
+        enemyEXP = 0;
+        enemyAttack = 30;
+        enemyDefense = 30;
+        enemyMaxMP = 0;
+        enemyMAttack = 30;
+        enemyMDefense = 30;
+        enemyAgility = 30;
+        enemyLuck = 30;
+        enemyMaxHP = 500;
+        enemyGold = 0;
+        Image = sp;
     }
 }
