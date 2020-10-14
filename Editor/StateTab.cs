6adc37cbc5f81abe6117c9fee96db9a251cb098b
ZivFerdinand <zivenf@gmail.com>
2020-10-14 17:50:25 +0700

    - Added: StateTab.cs
    - Added: StateTab.cs, Added Base Tab
    - Modified: TroopTab.cs, fixed some typing errors

---
+++ Editor/StateTab.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using SFB;
+public class StateTab : BaseTab
+{
+    //Textures
+    GUIStyle stateStyle;
+    GUIStyle columnStyle;
+    GUIStyle tabStyle;
+
+    public void OnRender(Rect position)
+    {
+        #region A Bit Explanation About Local Tab
+        ///So there is 2 types of Tab,
+        ///One is in Database that not included here.
+        ///Second, there is 3 tab slicing in TroopTab itself.
+        ///So make sure you understand that tabbing in here does not
+        ///have any corelation with DatabaseMain.cs Tab system.
+        #endregion
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////START REGION OF VALUE INIT/////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        float tabWidth = position.width * .85f;
+        float tabHeight = position.height - 10f;
+
+        float firstTabWidth = tabWidth * 3 / 10;
+
+        //Style area.
+        stateStyle = new GUIStyle(GUI.skin.box);
+        stateStyle.normal.background = CreateTexture(1, 1, Color.gray);
+        columnStyle = new GUIStyle(GUI.skin.box);
+        columnStyle.normal.background = CreateTexture(1, 1, new Color32(99, 100, 100, 200));
+        tabStyle = new GUIStyle(GUI.skin.box);
+        if (EditorGUIUtility.isProSkin)
+            tabStyle.normal.background = CreateTexture(1, 1, new Color32(76, 76, 76, 200));
+        else
+            tabStyle.normal.background = CreateTexture(1, 1, new Color32(200, 200, 200, 200));
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////END REGION OF VALUE INIT///////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        #region Entry Of StateTab GUILayout
+        //Start drawing the whole TroopTab.
+        GUILayout.BeginArea(new Rect(position.width / 7, 5, tabWidth, tabHeight));
+
+            //The black box behind the TroopTab? yes, this one.
+            GUILayout.Box(" ", stateStyle, GUILayout.Width(position.width - DatabaseMain.tabAreaWidth), GUILayout.Height(position.height - 25f));
+
+        GUILayout.EndArea(); //End drawing the whole StateTab
+        #endregion 
+
+
+    }
+
+    #region Features
+    public override void ItemTabLoader(int index)
+    {
+
+    }
+
+    #endregion
+}
--- Editor/TroopTab.cs
+++ Editor/TroopTab.cs
@@ -10,7 +10,7 @@ public class TroopTab : BaseTab
     public List<TroopData> troop = new List<TroopData>();
 
     //List of names. Why you ask? because selectionGrid require
-    //array of string, which we cannot obtain in troopData.
+    //array of string, which we cannot obtain in TroopData.
     //I hope later got better solution about this to not do
     //a double List for this kind of thing.
     List<string> troopDisplayName = new List<string>();
@@ -61,7 +61,7 @@ public class TroopTab : BaseTab
         #region A Bit Explanation About Local Tab
         ///So there is 2 types of Tab,
         ///One is in Database that not included here.
-        ///Second, there is 3 tab slicing in EnemyTab itself.
+        ///Second, there is 3 tab slicing in TroopTab itself.
         ///So make sure you understand that tabbing in here does not
         ///have any corelation with DatabaseMain.cs Tab system.
         #endregion
@@ -92,15 +92,15 @@ public class TroopTab : BaseTab
         ////////////////////////////////////////////////////////////////////////////////////////
 
         #region Entry Of TroopTab GUILayout
-        //Start drawing the whole EnemyTab.
+        //Start drawing the whole TroopTab.
         GUILayout.BeginArea(new Rect(position.width / 7, 5, tabWidth, tabHeight));
 
-            //The black box behind the troopsTab? yes, this one.
+            //The black box behind the TroopTab? yes, this one.
             GUILayout.Box(" ", troopStyle, GUILayout.Width(position.width - DatabaseMain.tabAreaWidth), GUILayout.Height(position.height - 25f));
 
             #region Tab 1/2
 
-            //First Tab of three
+            //First Tab of two
             GUILayout.BeginArea(new Rect(0, 0, tabWidth, tabHeight));
                 GUILayout.Box("Troops", GUILayout.Width(firstTabWidth), GUILayout.Height(position.height * .75f / 15));
 
@@ -218,7 +218,7 @@ public class TroopTab : BaseTab
 
         GUILayout.EndArea();
             #endregion
-        GUILayout.EndArea(); //End drawing the whole EnemyTab
+        GUILayout.EndArea(); //End drawing the whole TroopTab
         #endregion
 
         EditorUtility.SetDirty(troop[index]);
