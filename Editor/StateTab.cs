451bf3f029011b13d9a33366c32d7bcc0fafc24b
ZivFerdinand <zivenf@gmail.com>
2020-10-14 18:44:17 +0700

    - Modified: StateTab.cs
    - Modified: StateTab.cs, Added General Settings
    - Modified: StateData.cs, Added variables to save data
    
    Link: https://trello.com/c/QeiCMcZi/67-editor-tools-statestab-general-settings

--- Editor/StateTab.cs
+++ Editor/StateTab.cs
@@ -20,6 +20,33 @@ public class StateTab : BaseTab
     GUIStyle columnStyle;
     GUIStyle stateStyle;
 
+    //Arrays
+    public string[] stateRestriction =
+    {
+        "None",
+        "Attack On Enemy",
+        "Attack Anyone",
+        "Attack On Ally",
+        "Cannot Move",
+    };
+    public string[] stateSVMotion =
+    {
+        "Normal",
+        "Abnormal",
+        "Sleep",
+        "Dead",
+    };
+    public string[] stateSVOverlay =
+    {
+        "None",
+        "Poison",
+        "Blind",
+        "Silence",
+        "Rage",
+        "Confusion",
+        "Add Other Manually..",
+    };
+
     //How many state in ChangeMaximum Func
     public int stateSize;
     public int stateSizeTemp;
@@ -118,6 +145,138 @@ public class StateTab : BaseTab
 
             GUILayout.EndArea();
             #endregion // End Of First Tab
+
+            #region Tab 2/3
+            GUILayout.BeginArea(new Rect(firstTabWidth + 5, 0, firstTabWidth + 70, tabHeight - 25), columnStyle);
+
+                Rect generalBox = new Rect(5, 5, firstTabWidth + 60, position.height * .27f);
+                    #region GeneralSettings
+                    GUILayout.BeginArea(generalBox, tabStyle); // Start of General Settings tab
+                        GUILayout.Label("General Settings", EditorStyles.boldLabel); // General Settings label
+                        #region Vertical
+                        GUILayout.BeginVertical();
+                            #region Horizontal
+                            GUILayout.BeginHorizontal();
+                                #region Name
+                                GUILayout.BeginVertical();
+                                    GUILayout.Label("Name:"); // Name label
+                                    if (stateSize > 0)
+                                    {
+                                        state[index].stateName = GUILayout.TextField(state[index].stateName, GUILayout.Width(generalBox.width / 2 - 15), GUILayout.Height(generalBox.height / 8));
+                                        stateDisplayName[index] = state[index].stateName;
+                                    }
+                                    else
+                                    {
+                                        GUILayout.TextField("Null", GUILayout.Width(generalBox.width / 2 - 15), GUILayout.Height(generalBox.height / 8));
+                                    }
+                                GUILayout.EndVertical();
+        #endregion
+                                #region Icon
+                                Rect iconBox = new Rect(generalBox.width / 2 - 3, generalBox.height * .05f + 5, firstTabWidth - 220, position.height / 2);
+                                GUILayout.BeginArea(iconBox); // Icon Area
+                                GUILayout.BeginHorizontal();
+                                    GUILayout.BeginVertical();
+                                        GUILayout.Label("Icon:"); // Icon label
+                                    GUILayout.EndVertical();
+
+                                    GUILayout.BeginVertical();
+                                        GUILayout.Box(stateIcon, GUILayout.Width(61), GUILayout.Height(61)); // Icon Box preview
+                                        if (GUILayout.Button("Edit Icon", GUILayout.Height(20), GUILayout.Width(61))) // Icon changer Button
+                                        {
+                                            state[index].icon = ImageChanger(
+                                            index,
+                                            "Choose Icon",
+                                            "Assets/Resources/Image"
+                                            );
+                                            ItemTabLoader(index);
+                                        }
+                                    GUILayout.EndVertical();
+                                GUILayout.EndHorizontal();
+                                GUILayout.EndArea();
+                                #endregion
+                            GUILayout.EndHorizontal();
+                            #endregion
+
+                            GUILayout.Space(40);
+
+                            #region Horizontal
+                            GUILayout.BeginHorizontal();  
+        
+                                #region Restriction Priority
+                                GUILayout.BeginVertical();
+                                    GUILayout.Label("Restiction:"); // state Type class label
+                                    if (stateSize > 0)
+                                    {
+                                        state[index].selectedRestriction = EditorGUILayout.Popup(state[index].selectedRestriction, stateRestriction, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2 - 15));
+                                    }
+                                    else
+                                    {
+                                        EditorGUILayout.Popup(0, stateRestriction, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2 - 15));
+                                    }
+                                GUILayout.EndVertical();
+                                #endregion
+
+                                #region Priority
+                                GUILayout.BeginVertical();
+        
+                                    GUILayout.Label("Priority:"); // MP Cost class label
+                                    if (stateSize > 0)
+                                    { 
+                                        state[index].statePriority = EditorGUILayout.IntField(state[index].statePriority, 
+                                                                                                GUILayout.Width(generalBox.width / 4 - 2), 
+                                                                                                GUILayout.Height(generalBox.height / 8 - 9)
+                                                                                             ); 
+                                    }
+                                    else
+                                    { 
+                                        EditorGUILayout.IntField(-1, GUILayout.Width(generalBox.width / 4 - 2), GUILayout.Height(generalBox.height / 8 - 9)); 
+                                    }
+                                    GUILayout.BeginHorizontal();
+                                        GUILayout.Space(generalBox.width * .5f);
+                                    GUILayout.EndVertical();
+                                GUILayout.EndVertical();                  
+                                #endregion
+
+                            GUILayout.EndHorizontal();
+
+                            GUILayout.BeginHorizontal();
+
+                                #region [SV] Motion & Overlay
+                                GUILayout.BeginVertical();
+                                    GUILayout.Label("[SV] Motion:"); // Scope class label
+                                    if (stateSize > 0)
+                                    {
+                                        state[index].selectedSVMotion = EditorGUILayout.Popup(state[index].selectedSVMotion, stateSVMotion, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2 - 15));
+                                    }
+                                    else
+                                    {
+                                        EditorGUILayout.Popup(0, stateSVMotion, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2 - 15));
+                                    }
+                                GUILayout.EndVertical();
+
+                                GUILayout.BeginVertical();
+                                    GUILayout.Label("[SV] Overlay:"); // Occasion class label
+                                    if (stateSize > 0)
+                                    {
+                                        state[index].selectedSVOverlay = EditorGUILayout.Popup(state[index].selectedSVOverlay, stateSVOverlay, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2));
+                                    }
+                                    else
+                                    {
+                                        EditorGUILayout.Popup(0, stateSVOverlay, GUILayout.Height(generalBox.height / 8 - 15), GUILayout.Width(generalBox.width / 2));
+                                    }
+                                GUILayout.EndVertical();
+                                #endregion
+
+                            GUILayout.EndHorizontal();
+                            #endregion
+
+                        GUILayout.EndVertical();
+                        #endregion
+                    GUILayout.EndArea();
+                    #endregion
+
+            GUILayout.EndArea();
+            #endregion
 
         GUILayout.EndArea(); //End drawing the whole StateTab
         #endregion 
--- Scripts/StateData.cs
+++ Scripts/StateData.cs
@@ -1,11 +1,16 @@
-﻿using Boo.Lang;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Database/StateData")]
 public class StateData : ScriptableObject
 {
     public string stateName;
     public Sprite icon;
+
+    public int statePriority;
+    public int selectedRestriction;
+    public int selectedSVMotion;
+    public int selectedSVOverlay;
+
     [TextArea]
     public string notes;
 
