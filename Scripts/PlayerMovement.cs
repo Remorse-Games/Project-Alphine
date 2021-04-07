d3180fd8c775f898d5d3a06a6cc11886ca166863
ZivFerdinand <71594716+ZivFerdinand@users.noreply.github.com>
2021-04-07 13:20:52 +0700

    - Modified: PlayerMovement.cs
    Fixed Bug

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Customized Value")]
-    public float movementSpeed = 0;
+    [Header("Customizable Values")]
+    public float movementSpeed = .1f;
 
     private void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 0, 0));
-        transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical") * movementSpeed));
-
-        transform.GetChild(0).GetComponent<Animator>().SetFloat("horizontal", Input.GetAxis("Horizontal"));
-        transform.GetChild(0).GetComponent<Animator>().SetFloat("vertical", Input.GetAxis("Vertical"));
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0.00f, Input.GetAxis("Vertical")) * movementSpeed;
     }
 }
