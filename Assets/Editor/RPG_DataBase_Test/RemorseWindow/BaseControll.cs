70a6f494e5f6e4489c8c433a6f9907cc5a41e63f
imandana <imandanarahimaswara@gmail.com>
2021-04-09 22:05:01 +0700

    @ Simple Fix

---
+++ Assets/Editor/RPG_DataBase_Test/RemorseWindow/BaseControll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace RemorseWindow
+{
+    public class BaseControll
+    {
+        public BaseControll(EditorWindow currentEditorWindow, BaseControll parent, String name, Rect rect)
+        {
+            m_editorWindow = currentEditorWindow;
+            this.parent = parent;
+            this.name =  name;
+            
+            this.rect =  (this.parent != null)? 
+                    new Rect( parent.rect.position + rect.position, rect.size) : rect;
+        }
+        
+        public String name;
+        public EditorWindow m_editorWindow ;
+       /*  public Vector2 position; */
+        public Rect rect;
+        public BaseControll parent;
+        
+        protected List<bool> condition;
+        public List<Action> listEvents;
+        
+        public virtual void Draw()
+        { }
+        protected virtual void ExecuteEvents()
+        {
+        }
+    }
+}
---
+++ Assets/Editor/RPG_DataBase_Test/RemorseWindow/Panel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace RemorseWindow
+{
+    public class Panel : BaseControll
+    {
+        public Panel(EditorWindow currentEditorWindow, BaseControll parent, String name, Rect rect)
+        : base(currentEditorWindow, parent, name, rect)
+        {
+        }
+        
+        public override void Draw()
+        {
+            GUILayout.BeginArea(rect);
+            GUILayout.Box(name, GUILayout.Width(rect.width), GUILayout.Height(rect.height));
+            GUILayout.EndArea();
+        }
+    }
+}
