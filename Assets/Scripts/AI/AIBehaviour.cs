9462c35076e531debc3a36bc9ac521f7b84feb96
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-04-25 19:29:02 +0700

    Update AIBehaviour.cs
    Vision Gizmos

--- Assets/Scripts/AI/AIBehaviour.cs
+++ Assets/Scripts/AI/AIBehaviour.cs
@@ -23,8 +23,13 @@ namespace Remorse.AI
         public Transform[] patrolArea;
         [HideInInspector] public int index;
 
+        [Header("Vision")]
+        [Range(0, 20)] public float radius;
+        [Range(0, 360)] public float fov;
+
         //[HideInInspector] public FieldOfView fov;
         private float tempCooldown;
+
         private void Start()
         {
 
@@ -50,10 +55,56 @@ namespace Remorse.AI
 
         public void Combat()
         {
+
         }
 
         private void Attack()
         {
+
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // draw circle gizmo
+            float defaultDeg = 360 / 36;
+
+            for (int i = 0; i < 36; i++)
+            {
+                float deg = defaultDeg * i * Mathf.Deg2Rad;
+                float nextDeg = defaultDeg * (i == 36 ? 0 : i + 1) * Mathf.Deg2Rad;
+
+                Vector3 pos = new Vector3(
+                    Mathf.Sin(deg) * radius,
+                    0,
+                    Mathf.Cos(deg) * radius
+                );
+
+                Vector3 nextPos = new Vector3(
+                    Mathf.Sin(nextDeg) * radius,
+                    0,
+                    Mathf.Cos(nextDeg) * radius
+                );
+
+                Gizmos.DrawLine(pos + transform.position, nextPos + transform.position);
+            }
+
+            // draw FOV
+            defaultDeg = fov / 2;
+
+            Vector3 rPos = new Vector3(
+                Mathf.Sin(Mathf.Deg2Rad * defaultDeg) * radius,
+                0,
+                Mathf.Cos(Mathf.Deg2Rad * defaultDeg) * radius
+            );
+
+            Vector3 lPos = new Vector3(
+                Mathf.Sin(Mathf.Deg2Rad * (360 - defaultDeg)) * radius,
+                0,
+                Mathf.Cos(Mathf.Deg2Rad * (360 - defaultDeg)) * radius
+            );
+
+            Gizmos.DrawLine(transform.position, rPos + transform.position);
+            Gizmos.DrawLine(transform.position, lPos + transform.position);
         }
     }
 }
