4265771bbf9956aab0ae90f6dc684291eb3d11d6
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-04-28 21:11:14 +0700

    Change GridSystem name to GridVector

---
+++ Assets/Scripts/Utils/GridVector.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Remorse.Utility
+{
+    public class GridVector
+    {
+        private float _x, _y;
+
+        public float x
+        {
+            get { return _x; }
+            set
+            {
+                _x = Mathf.Round(value);
+            }
+        }
+
+        public float y
+        {
+            get { return _y; }
+            set
+            {
+                _y = Mathf.Round(value);
+            }
+        }
+
+        public GridVector(float x, float y)
+        {
+            _x = Mathf.Round(x);
+            _y = Mathf.Round(y);
+        }
+
+        public GridVector(Vector2 pos)
+        {
+            _x = Mathf.Round(pos.x);
+            _y = Mathf.Round(pos.y);
+        }
+
+        public GridVector(Vector3 pos)
+        {
+            _x = Mathf.Round(pos.x);
+            _y = Mathf.Round(pos.y);
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(_x, 0, _y);
+        }
+
+        public Vector2 ToVector2()
+        {
+            return new Vector3(_x, _y);
+        }
+
+        public static implicit operator Vector3(GridVector grid) => new Vector3(grid.x, 0, grid.y);
+        public static implicit operator Vector2(GridVector grid) => new Vector2(grid.x, grid.y);
+
+        public static GridVector operator +(GridVector a, GridVector b) => new GridVector(a.x + b.x, a.y + b.y);
+        public static GridVector operator -(GridVector a, GridVector b) => new GridVector(a.x - b.x, a.y - b.y);
+        public static GridVector operator *(GridVector a, GridVector b) => new GridVector(a.x * b.x, a.y * b.y);
+        public static GridVector operator /(GridVector a, GridVector b)
+        {
+            if (b.x == 0 || b.y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return new GridVector(a.x / b.x, a.y / b.y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", _x, _y);
+        }
+    }
+
+}
