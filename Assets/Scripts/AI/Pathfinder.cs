561ce12d830b49396d3b7916ee87897d983d89bf
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-04-28 14:22:42 +0700

    Change AIPathFinder name to Pathfinder

---
+++ Assets/Scripts/AI/Pathfinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Remorse.AI
+{
+    public static class Pathfinder
+    {
+
+        public static Vector3[] FindPath(Vector3 currentPos, Vector3 destination)
+        {
+            Vector3[] result =
+            {
+                currentPos,
+                new Vector3(currentPos.x, (currentPos.y + destination.z) / 2, destination.z),
+                destination
+            };
+
+            return result;
+        }
+
+    }
+}
