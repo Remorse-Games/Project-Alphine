6da38a50d981bd24ef47fa1d254d9603936b7ace
ZivFerdinand <71594716+ZivFerdinand@users.noreply.github.com>
2021-05-10 14:52:43 +0700

    - Added: Payment Scene
    Warning: Not a Final Version, Revision Accepted. This Payment scene is using TextMeshPro, please import first

---
+++ Assets/Scripts/Game/Payment/ButtonFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    EconomySystem myValues;
+    void Start()
+    {
+        myValues = GetComponent<EconomySystem>();
+    }
+
+    public void Earn50()
+    {
+        myValues.SpendOrEarnMoney(50);
+    }
+    public void Earn60()
+    {
+        myValues.SpendOrEarnMoney(60);
+    }
+    public void Earn70()
+    {
+        myValues.SpendOrEarnMoney(70);
+    }
+    public void Item80()
+    {
+        myValues.SpendOrEarnMoney(-80);
+    }
+    public void Item90()
+    {
+        myValues.SpendOrEarnMoney(-90);
+    }
+    public void Item100()
+    {
+        myValues.SpendOrEarnMoney(-100);
+    }
+}
---
+++ Assets/Scripts/Game/Payment/EconomySystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EconomySystem : MonoBehaviour
+{
+    int money;
+    // Start is called before the first frame update
+    TextMeshProUGUI moneyStatus;
+    void Start()
+    {
+        Destroy(GameObject.Find("Warning Text"));
+        money = PlayerPrefs.GetInt("Money");
+        moneyStatus = GameObject.Find("Money Status").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyStatus.text = "Money: $" + money.ToString();
+    }
+
+    public void SpendOrEarnMoney(int _money)
+    {
+        if (money + _money >= 0)
+            money += _money;
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetInt("Money", money);
+        PlayerPrefs.Save();
+    }
+}
