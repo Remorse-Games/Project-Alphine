b1b510684feaa64e51692345908f1dd36308588f
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2021-07-04 18:57:46 +0700

    - Modified: PathFinderTest.cs
    Add More Test

--- Assets/Scripts/Test/EditMode/PathFinderTest.cs
+++ Assets/Scripts/Test/EditMode/PathFinderTest.cs
@@ -21,10 +21,10 @@ namespace Tests
             CollectionAssert.AreEqual(
                 expected: new List<GridVector>
                 {
-                    new GridVector(0, 0, 0),
-                    new GridVector(1, 0, 0),
-                    new GridVector(2, 0, 0),
-                    new GridVector(3, 0, 0)
+                    new GridVector( 0,  0,  0),
+                    new GridVector( 1,  0,  0),
+                    new GridVector( 2,  0,  0),
+                    new GridVector( 3,  0,  0)
                 },
                 actual: ActualResult
             );
@@ -38,11 +38,138 @@ namespace Tests
             CollectionAssert.AreEquivalent(
                 expected: new List<GridVector>
                 {
-                    new GridVector(0, 0, 0),
-                    new GridVector(0, 0, 1),
-                    new GridVector(0, 0, 2),
-                    new GridVector(1, 0, 2),
-                    new GridVector(2, 0, 2)
+                    new GridVector( 0,  0,  0),
+                    new GridVector( 0,  0,  1),
+                    new GridVector( 0,  0,  2),
+                    new GridVector( 1,  0,  2),
+                    new GridVector( 2,  0,  2)
+                },
+                actual: ActualResult
+            );
+        }
+
+        [Test]
+        public void Case3()
+        {
+            path.FindPath(new GridVector(), new GridVector(-2, 0, 2));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector( 0,  0,  0),
+                    new GridVector( 0,  0,  1),
+                    new GridVector( 0,  0,  2),
+                    new GridVector(-1,  0,  2),
+                    new GridVector(-2,  0,  2)
+                },
+                actual: ActualResult
+            );
+        }
+
+        [Test]
+        public void Case4()
+        {
+            path.FindPath(new GridVector(), new GridVector(-2, 0, -2));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector( 0,  0,  0),
+                    new GridVector( 0,  0, -1),
+                    new GridVector( 0,  0, -2),
+                    new GridVector(-1,  0, -2),
+                    new GridVector(-2,  0, -2)
+                },
+                actual: ActualResult
+            );
+        }
+
+        [Test]
+        public void Case5()
+        {
+            path.FindPath(new GridVector(-2, 0, 0), new GridVector(2, 0, 2));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector(-2,  0,  0),
+                    new GridVector(-2,  0,  1),
+                    new GridVector(-2,  0,  2),
+                    new GridVector(-1,  0,  2),
+                    new GridVector( 0,  0,  2),
+                    new GridVector( 1,  0,  2),
+                    new GridVector( 2,  0,  2)
+                },
+                actual: ActualResult
+            );
+        }
+
+
+        [Test]
+        public void Case6()
+        {
+            path.FindPath(new GridVector(-2, 0, -2), new GridVector(2, 0, 2));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector(-2,  0, -2),
+                    new GridVector(-2,  0, -1),
+                    new GridVector(-2,  0,  0),
+                    new GridVector(-2,  0,  1),
+                    new GridVector(-2,  0,  2),
+                    new GridVector(-1,  0,  2),
+                    new GridVector( 0,  0,  2),
+                    new GridVector( 1,  0,  2),
+                    new GridVector( 2,  0,  2)
+                },
+                actual: ActualResult
+            );
+        }
+
+        [Test]
+        public void Case7()
+        {
+            path.FindPath(new GridVector(-2, 0, -2), new GridVector(-4, 0, 4));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector(-2,  0, -2),
+                    new GridVector(-2,  0, -1),
+                    new GridVector(-2,  0,  0),
+                    new GridVector(-2,  0,  1),
+                    new GridVector(-2,  0,  2),
+                    new GridVector(-2,  0,  3),
+                    new GridVector(-2,  0,  4),
+                    new GridVector(-3,  0,  4),
+                    new GridVector(-4,  0,  4)
+                },
+                actual: ActualResult
+            );
+        }
+
+        [Test]
+        public void Case8()
+        {
+            path.FindPath(new GridVector(2, 0, -2), new GridVector(-4, 0, 4));
+            List<GridVector> ActualResult = path;
+            CollectionAssert.AreEquivalent(
+                expected: new List<GridVector>
+                {
+                    new GridVector( 2,  0, -2),
+                    new GridVector( 2,  0, -1),
+                    new GridVector( 2,  0,  0),
+                    new GridVector( 2,  0,  1),
+                    new GridVector( 2,  0,  2),
+                    new GridVector( 2,  0,  3),
+                    new GridVector( 2,  0,  4),
+                    new GridVector( 1,  0,  4),
+                    new GridVector( 0,  0,  4),
+                    new GridVector(-1,  0,  4),
+                    new GridVector(-2,  0,  4),
+                    new GridVector(-3,  0,  4),
+                    new GridVector(-4,  0,  4)
                 },
                 actual: ActualResult
             );
